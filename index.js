78929e66d23a8b414dbaec6a8aafde3084a36126
Owusu-Ansah Solomon <owusuansahsolomon39@gmail.com>
2023-06-04 09:03:23 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from 'dotenv';
+
+const app = express();
+
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+
+app.get("/", (req, res) => {
+  res.send("WELCOME TO THE VIRTUAL CLASSROOM BACKEND!");
+});
