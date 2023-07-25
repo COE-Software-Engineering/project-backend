4f7925896a1cf671e8f086723d31fcdb34510e1e
e-hokage777 <e.hokage777@gmail.com>
2023-07-25 15:29:00 +0000

    changed database connection to connection string

--- models/postgresql.js
+++ models/postgresql.js
@@ -5,12 +5,15 @@ const { Pool } = pg;
 // generating env variables
 dotenv.config();
 
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+// });
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DB_CONNECTION_STRING
 });
 
 // export default pool;
